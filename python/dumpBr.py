e53e9aee4f59dbbe0cb275a486f5d1c4fe6701d6
Rohin Thampilali Narayan <narayan@lxplus439.cern.ch>
2012-05-15 18:45:23 +0200

    Cosmetic changes

--- python/dumpBr.py
+++ python/dumpBr.py
@@ -1,6 +1,8 @@
 import ROOT,sys
-
-f   = ROOT.TFile(sys.argv[1])
-t   = f.Get("el")
-
-t.Show()
+try:
+    print "The list of branches in the root file passed as argument"
+    f   = ROOT.TFile(sys.argv[1])
+    t   = f.Get("el")
+    t.Show()
+except:
+    print "No root file passed as argument"
