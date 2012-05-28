31cd1f872caaaf063474a43d2baa845b53af3332
Rohin Thampilali Narayan <narayan@lxplus420.cern.ch>
2012-05-28 16:17:01 +0200

    Grid submission input file ZeeNp5

--- share/gridsub.py
+++ share/gridsub.py
@@ -4,18 +4,18 @@ myDatasets  = [\
         #'mc11_7TeV.109302.AlpgenJimmyZeebbNp2_nofilter.merge.AOD.e835_s1310_s1300_r3043_r2993/'\
         #'mc11_7TeV.109303.AlpgenJimmyZeebbNp3_nofilter.merge.AOD.e835_s1310_s1300_r3043_r2993/'\
         #'mc11_7TeV.107650.AlpgenJimmyZeeNp0_pt20.merge.AOD.e835_s1299_s1300_r3043_r2993/'\
-        'mc11_7TeV.107651.AlpgenJimmyZeeNp1_pt20.merge.AOD.e835_s1299_s1300_r3043_r2993/'\
+        #'mc11_7TeV.107651.AlpgenJimmyZeeNp1_pt20.merge.AOD.e835_s1299_s1300_r3043_r2993/'\
         #'mc11_7TeV.107652.AlpgenJimmyZeeNp2_pt20.merge.AOD.e835_s1299_s1300_r3043_r2993/'\
         #'mc11_7TeV.107653.AlpgenJimmyZeeNp3_pt20.merge.AOD.e835_s1299_s1300_r3043_r2993/'\
         #'mc11_7TeV.107654.AlpgenJimmyZeeNp4_pt20.merge.AOD.e835_s1299_s1300_r3043_r2993/'\
-        #'mc11_7TeV.107655.AlpgenJimmyZeeNp5_pt20.merge.AOD.e835_s1299_s1300_r3043_r2993/'\
+        'mc11_7TeV.107655.AlpgenJimmyZeeNp5_pt20.merge.AOD.e835_s1299_s1300_r3043_r2993/'\
         ]
 
 j = Job()
 j.application = Athena()
 j.application.atlas_dbrelease = 'LATEST'
 j.application.option_file = ['AnalysisMaster.py' ]
-j.name = 'ZeebbNp0'
+j.name = 'ZeeNp5'
 j.application.prepare()
 j.inputdata = DQ2Dataset()
 j.inputdata.dataset = myDatasets
