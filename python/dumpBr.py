ac495d06588a5d174b64cec675a98c8a6b3dd0ef
Rohin Thampilali Narayan <narayan@lxplus421.cern.ch>
2012-08-01 18:34:42 +0200

    First commit of elID branch

--- python/dumpBr.py
+++
@@ -1,8 +0,0 @@
-import ROOT,sys
-try:
-    print "The list of branches in the root file passed as argument"
-    f   = ROOT.TFile(sys.argv[1])
-    t   = f.Get("el")
-    t.Show()
-except:
-    print "No root file passed as argument"
