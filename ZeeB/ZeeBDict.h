a2c8cfe141fe323d43ea53aa21063531cf71e55a
Rohin Narayan <narayan@higgs.physi.uni-heidelberg.de>
2012-10-26 16:18:41 +0200

    first commit

--- ZeeB/ZeeBDict.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZEEB_ZEEBDICT_h
-#define ZeeB_ZeeBDict_h
-
-#include "egammaEvent/ElectronContainer.h"
-#include "egammaEvent/Electron.h"
-#include "egammaEvent/ElectronAssociation.h"
-
-#endif 
-
--- src/components/ZeeB_entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "ZeeB/SoftElectron.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_ALGORITHM_FACTORY( SoftElectron )
-
-DECLARE_FACTORY_ENTRIES( ZeeB ) 
-{
-    DECLARE_ALGORITHM( SoftElectron )
-}
